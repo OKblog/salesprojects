9c98b759cc40638d62ae102a27cc045bac8cf995
OKblog <otsukikazuki@outlook.com>
2025-06-15 22:08:32 +0900

    settings.py re2

--- salesproject/settings.py
+++ salesproject/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = BASE_DIR / "static"
+STATIC_URL = str(BASE_DIR / "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
