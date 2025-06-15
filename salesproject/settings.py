d87bda443a4f64210ce9ae515026f4fa81cbfc11
OKblog <otsukikazuki@outlook.com>
2025-06-15 22:39:27 +0900

    settings.py re14

--- salesproject/settings.py
+++ salesproject/settings.py
@@ -18,8 +18,8 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.join(__file__, '../../../'))
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_URL = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
